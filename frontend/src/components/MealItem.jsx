b0dfa90778c2b21394c28b91af2c083c81f735c6
Tarik Pazalja <tarikpazalja@gmail.com>
2023-10-26 14:02:29 +0200

    gfhfhgfghf

--- frontend/src/components/MealItem.jsx
+++ frontend/src/components/MealItem.jsx
@@ -14,7 +14,7 @@ export default function MealItem({ meal }) {
   return (
     <li className="meal-item">
       <article>
-        <img src={`https://node-u4mu.onrender.com//${meal.image}`} alt={meal.name} />
+        <img src={`https://node-u4mu.onrender.com/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
           <p className="meal-item-price">
